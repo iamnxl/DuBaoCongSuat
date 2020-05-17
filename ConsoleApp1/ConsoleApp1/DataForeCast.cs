72b269ed5bf2a276cab08fa996f8f2621d47682c
iamnxl <65488854+iamnxl@users.noreply.github.com>
2020-05-17 17:05:15 +0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/DataForeCast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DataForeCast
+    {
+        public string date { get; set; }
+        public double air_temperature { get; set; }
+        public double ghi { get; set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/DataHistory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DataHistory
+    {
+        public string time { get; set; }
+        public double capacity { get; set; }
+        public double ghi { get; set; }
+        public double paneltemp { get; set; }
+        public double envtemp { get; set; }
+
+    }
+    class SortData : Comparer<DataHistory>
+    {
+        public override int Compare(DataHistory x, DataHistory y)
+        {
+            if (x.ghi > y.ghi)
+            {
+                return 1;
+            }
+            else if (x.ghi == y.ghi)
+            {
+                if (x.envtemp > y.envtemp)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
